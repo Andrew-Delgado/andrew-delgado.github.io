5d7f3c02c7c5a05a8c59b604c12572145a1115f6
Andrew Delgado <andrewdelgado017@gmail.com>
2024-02-11 22:49:18 -0800

    added css variables and italics

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
 function changeBackgroundColor() {
-  let currentTime = new Date();
-  let hours = currentTime.getHours();
+  const currentTime = new Date();
+  const hours = currentTime.getHours();
 
-  let currentShade = "#B38C00"; // day
-  let currentFontColor = "#000000"; // day
-  
-  if (hours < 6 && hours > 14) {
-    currentShade = "#343540"; // night
-    currentFontColor = "#B38C00"; // night
-  }
+  const dayBackgroundColor = "#B38C00"
+  const dayTextColor = "#000000"
 
-  document.body.style.backgroundColor = currentShade;
-  const elementsToUpdate = ["a", "p", "header"]
-  let all = []
+  const nightBackgroundColor = "#343540"
+  const nightTextColor = "#B38C00"
 
-  elementsToUpdate.forEach((e) => {
-    all = [...all, ...document.getElementsByTagName(e)]
-  });
-
-  for (let i = 0, max = all.length; i < max; i++) {
-    all[i].style.color = currentFontColor;
+  if (hours < 6 || hours > 14) {
+    document.documentElement.style.setProperty("--background-color", nightBackgroundColor)
+    document.documentElement.style.setProperty("--font-color", nightTextColor)
+  } else {
+    document.documentElement.style.setProperty("--background-color", dayBackgroundColor)
+    document.documentElement.style.setProperty("--font-color", dayTextColor)
   }
 }
 
-window.onload = changeBackgroundColor;
+function italicCurrentPage() {
+  const currentPageTitle = document.title
+  const id = document.getElementById(currentPageTitle.concat(' link'));
+  id.style.fontStyle = "italic"
+}
+
+// This is not complete. 
+// I think tapping the Header would be an easy way to test color changes.
+function forceColorChange() {
+  // Select the header element
+  const header = document.getElementById('header');
+
+  // Initialize tap counter
+  let tapCount = 0;
+
+  // Add event listener for 'click' event
+  header.addEventListener('click', () => {
+    tapCount++; // Increment tap count
+    console.log(`Tapped ${tapCount} times.`);
+  });
+}
+
+function fontAndColorUpdates() {
+  changeBackgroundColor()
+  italicCurrentPage()
+}
+
+document.addEventListener('DOMContentLoaded', fontAndColorUpdates);
